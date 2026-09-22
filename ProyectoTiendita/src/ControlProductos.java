16e43e08c7eca79aca1356b7849b80b866e86c82
TuPapiJorgito <jorgemireles443@gmail.com>
2026-09-22 01:07:26 -0600

    Implementa funcion buscar producto

--- ProyectoTiendita/src/ControlProductos.java
+++ ProyectoTiendita/src/ControlProductos.java
@@ -1,6 +1,7 @@
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class ControlProductos {
     
@@ -21,8 +22,24 @@ public class ControlProductos {
     }
 
     void buscar() {
-        throw new UnsupportedOperationException("En construcción."); 
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Ingresa el código del producto a buscar: ");
+    int codigoBuscado = sc.nextInt();
+
+    boolean encontrado = false;
+
+    for (Producto p : listaProductos) {
+        if (p.getCodigo() == codigoBuscado) {
+            System.out.println("Encontrado: " + p);
+            encontrado = true;
+            break;
+        }
     }
+
+    if (!encontrado) {
+        System.out.println("No se encontró ningún producto con ese código.");
+    }
+}
     
     
 }
